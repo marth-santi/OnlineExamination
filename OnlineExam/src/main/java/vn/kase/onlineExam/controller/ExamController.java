a4c86ee0b3e261add993167854fb306c5cb56ea7
dang <artesuto@gmail.com>
2021-01-14 23:18:00 +0700

    complete trigger save exam from controller

--- OnlineExam/src/main/java/vn/kase/onlineExam/controller/ExamController.java
+++ OnlineExam/src/main/java/vn/kase/onlineExam/controller/ExamController.java
@@ -1,5 +1,8 @@
 package vn.kase.onlineExam.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +11,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import vn.kase.onlineExam.model.Question;
+import vn.kase.onlineExam.model.Subject;
+import vn.kase.onlineExam.repository.QuestionRepository;
+import vn.kase.onlineExam.repository.SubjectRepository;
 import vn.kase.onlineExam.viewModel.ExamEditorVM;
 
 @RestController
 public class ExamController {
+	
+	@Autowired
+	private QuestionRepository questionRepo;
+	@Autowired
+	private SubjectRepository subjectRepo;
 
 	// === VIEW RETURN SECTION ===
 	
@@ -33,6 +45,17 @@ public class ExamController {
 	@PostMapping("/exam")
 	@ResponseBody
 	public ExamEditorVM saveExam(@RequestBody ExamEditorVM examPlan) {
-		return examPlan;
+		Subject savedSubject = subjectRepo.save(examPlan.getSubject());
+		for (Question question : examPlan.getQuestions()) {
+			question.setSubjectId(savedSubject.getId());
+		}
+		List<Question> savedQuestions = (List<Question>) questionRepo.saveAll(examPlan.getQuestions());
+		
+		ExamEditorVM savedExam = new ExamEditorVM();
+		savedExam.setQuestions(savedQuestions);
+		savedExam.setSubject(savedSubject);
+		
+		return savedExam;
 	}
 }
+
--- OnlineExam/src/main/java/vn/kase/onlineExam/model/Subject.java
+++ OnlineExam/src/main/java/vn/kase/onlineExam/model/Subject.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -24,7 +25,7 @@ public class Subject implements Serializable {
 
 	@Id
     @Column(name = "id")
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "subject_name")
