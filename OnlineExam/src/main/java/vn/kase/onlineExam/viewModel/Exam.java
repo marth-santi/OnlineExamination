1468de010de8ee7a43a4b05f042fe03504bb1c2f
Duc <artesuto@gmail.com>
2021-01-17 23:21:45 +0700

    split model object to binding fragment success

--- OnlineExam/src/main/java/vn/kase/onlineExam/viewModel/Exam.java
+++ OnlineExam/src/main/java/vn/kase/onlineExam/viewModel/Exam.java
@@ -1,5 +1,6 @@
 package vn.kase.onlineExam.viewModel;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
@@ -7,7 +8,11 @@ import vn.kase.onlineExam.model.Question;
 import vn.kase.onlineExam.model.Subject;
 
 @Data
-public class Exam {
+public class Exam implements Serializable{
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 	private Subject subject;
 	private List<Question> questions;
 }
