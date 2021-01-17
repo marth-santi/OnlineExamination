fcfdaed2a3569f8ec88dc8a9f8d7f11c04b7bc55
Duc <artesuto@gmail.com>
2021-01-17 11:13:59 +0700

    update subject fragment layout

--- OnlineExam/src/main/java/vn/kase/onlineExam/controller/ExamController.java
+++ OnlineExam/src/main/java/vn/kase/onlineExam/controller/ExamController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,7 +28,7 @@ public class ExamController {
 	public ModelAndView createExam() {
 		ModelAndView modelView = new ModelAndView();
 		modelView.setViewName("exam/editor");
-		
+
 		Exam exam = new Exam();
 		// Mock data
 		Subject subject = new Subject();
@@ -35,9 +37,15 @@ public class ExamController {
 		exam.setQuestions(questions);
 		exam.setSubject(subject);
 		// End mock data
-		
+
 		modelView.addObject(exam);
 		return modelView;
+	}
+	
+	@PostMapping("/exam/submitExam")
+	public String submitExam(@ModelAttribute(value = "exam") Exam exam) {
+		System.out.println(exam.toString());
+		return "success";
 	}
 		
 }
