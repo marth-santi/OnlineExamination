e2b0ba7b74a1d3aaafae472a1a7fc7bd6eb72931
Duc <artesuto@gmail.com>
2021-01-19 23:25:38 +0700

    fix conflict rest mapping

--- OnlineExam/src/main/java/vn/kase/onlineExam/controller/EditorController.java
+++ OnlineExam/src/main/java/vn/kase/onlineExam/controller/EditorController.java
@@ -1,10 +1,12 @@
 package vn.kase.onlineExam.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("/editor")
 public class EditorController {
 
 	@GetMapping("/question/add")
