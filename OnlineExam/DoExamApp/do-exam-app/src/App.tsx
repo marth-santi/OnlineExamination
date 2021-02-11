ed0f63cab029287f40d78d50c12831100450a328
Duc <artesuto@gmail.com>
2021-02-11 18:05:22 +0700

    success get data from spring controller

--- OnlineExam/DoExamApp/do-exam-app/src/App.tsx
+++ OnlineExam/DoExamApp/do-exam-app/src/App.tsx
@@ -4,22 +4,32 @@ import CONSTANT from "./CONST";
 
 function App() {
   const [data, setData] = useState("");
+  const [data2, setData2] = useState("");
   useEffect(() => {
     fetch(CONSTANT.api.test)
       .then(
         async (res: Response): Promise<string> => {
-          //console.log(res.text());
           return res.text();
         }
       )
       .then((data: string): void => {
         setData(data);
       });
+    fetch(CONSTANT.api.getExam)
+      .then(
+        async (res: Response): Promise<string> => {
+          return res.json();
+        }
+      )
+      .then((data2: any): void => {
+        console.log(data2);
+      });
   });
   return (
     <div className="App">
       Hello from React App
       <p>{data}</p>
+      <p>{data2}</p>
     </div>
   );
 }
--- OnlineExam/DoExamApp/do-exam-app/src/CONST.tsx
+++ OnlineExam/DoExamApp/do-exam-app/src/CONST.tsx
@@ -1,6 +1,7 @@
 const CONST = {
   api: {
     test: "//localhost:8080/api/students/test",
+    getExam: "//localhost:8080/api/students/getExam",
   },
 };
 
